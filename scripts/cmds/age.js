d2227abf01d03236e6dc044eab98e1cc6fa5199b
yourbaby646-stack <yourbaby646@gmail.com>
2026-03-26 14:15:05 +0600

    Update age.js

--- scripts/cmds/age.js
+++ scripts/cmds/age.js
@@ -10,7 +10,7 @@ module.exports = {
                 name: "age",
                 aliases: ["বয়স"],
                 version: "1.7",
-                author: "MahMUD",
+                author: "ADIL",
                 countDown: 5,
                 role: 0,
                 description: {
@@ -29,15 +29,15 @@ module.exports = {
         langs: {
                 bn: {
                         noInput: "× বেবি, তোমার জন্ম তারিখ দাও!\n\nউদাহরণ: {pn} 2002-05-15",
-                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact MahMUD।"
+                        error: "× সমস্যা হয়েছে: %1। প্রয়োজনে Contact NA RU TO ।"
                 },
                 en: {
                         noInput: "× Baby, please provide your date of birth\n\nExample: {pn} 2002-05-15",
-                        error: "× API error: %1. Contact MahMUD for help."
+                        error: "× API error: %1. Contact NA RU TO for help."
                 },
                 vi: {
                         noInput: "× Cưng ơi, vui lòng cung cấp ngày sinh\n\nVí dụ: {pn} 2002-05-15",
-                        error: "× Lỗi: %1. Liên hệ MahMUD để hỗ trợ."
+                        error: "× Lỗi: %1. Liên hệ NA RU TO  để hỗ trợ."
                 }
         },
 
