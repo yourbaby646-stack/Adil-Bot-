9a519e60aa0b20fbf4091ca46ef6e8276208460e
yourbaby646-stack <yourbaby646@gmail.com>
2026-03-26 13:33:49 +0600

    Update admin.js

--- scripts/cmds/admin.js
+++ scripts/cmds/admin.js
@@ -5,7 +5,7 @@ module.exports = {
         config: {
                 name: "admin",
                 version: "1.7",
-                author: "MahMUD",
+                author: "ADIL",
                 countDown: 5,
                 role: 2,
                 description: {
